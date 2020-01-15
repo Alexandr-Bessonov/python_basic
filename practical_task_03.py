2f4d629d8104e356350b136736f5e352138b04ae
Bessonov Alexandr <sabe84@mail.ru>
2020-01-15 14:52:22 +0300

    практическое задание №3 выполнено

--- practical_task_03.py
+++ practical_task_03.py
@@ -1,3 +1,8 @@
 print("This is the solution to task 3")
 
 n = input("Введите любое число n: ")
+
+nn = int(str(n) + str(n))
+nnn = int(str(n) + str(n) + str(n))
+
+print(f"На основании того, что Вы ввели n = {n}, выражение n + nn + nnn = {int(n) + nn + nnn}")
