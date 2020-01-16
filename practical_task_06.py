b81a0ff4a6e048b9952f387a8f609c7d0ba374f0
Bessonov Alexandr <sabe84@mail.ru>
2020-01-16 12:13:54 +0300

    почему то не работает правильно цикл. Надо разбираться

--- practical_task_06.py
+++ practical_task_06.py
@@ -1,1 +1,18 @@
 print("This is the solution to task 6")
+
+print('Приветствую тебя, спортсмен!')
+name = input('Введи твоё имя: ')
+start = int(input(f'{name}, сколько ты сейчас пробегаешь киллометров в день? (нужно указать целое число) '))
+finish = int(input(f'Укажи к какому результату (в км) ты стремишься: '))
+result = 0
+while True:
+    if start < finish:
+        start = start * 1.1
+        result += 1
+    else:
+        print('Ты уже достиг запланированного результата')
+        break
+
+print(f'{name}, тебе понадобиться {result} дней для достижения цели в {finish} км.')
+
+print(f'{result} - ответ одним натуральным числом - номер дня (как в задании)')
