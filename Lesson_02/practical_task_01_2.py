2291c64fda212a4f14a93d44750ae7db6ef00d9d
Bessonov Alexandr <sabe84@mail.ru>
2020-01-19 10:37:13 +0300

    Практическое задание №1 ко второму уроку выполнено

--- Lesson_02/practical_task_01_2.py
+++ Lesson_02/practical_task_01_2.py
@@ -1,1 +1,16 @@
 print("This is the solution to task 1 to lesson 2")
+
+result_list = [26, False, None, "Hello", 74.45]
+print(result_list)
+
+length = len(result_list)  # переменная введена для случая, если список вводит пользователь
+
+var = 0
+
+while var < length:
+    var_type = result_list[var]
+    print(var_type)
+    print(type(var_type))
+    var += 1
+
+print("End")
