1ac417dd0daa997fe6011aa98f27e27b78dfe9ed
Bessonov Alexandr <sabe84@mail.ru>
2020-01-19 12:38:24 +0300

    Практическая задача №4 по выведению пронумерованного списка решена, осталось решить с длинной более 10 символов

--- Lesson_02/practical_task_04_2.py
+++ Lesson_02/practical_task_04_2.py
@@ -1,1 +1,9 @@
 print("This is the solution to task 4 to lesson 2")
+
+user_str = "Hello 123456789012345 world"
+
+str_work = user_str.split(" ")
+print(str_work)
+
+for ind, el in enumerate(str_work, 1):
+    print(ind, el)
