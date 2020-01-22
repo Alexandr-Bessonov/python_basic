c2afb58cba779fede819cd93aa669b69ade1c100
Bessonov Alexandr <sabe84@mail.ru>
2020-01-22 12:14:49 +0300

    Первое практическое задание к уроку №3 выполнено

--- Lesson_03/practical_task_01_3.py
+++ Lesson_03/practical_task_01_3.py
@@ -1,2 +1,19 @@
-print("This is the solution to task 5 to lesson 2")
+print("This is the solution to task 1 to lesson 3")
 print('_' * 150)
+print('Реализовать функцию, принимающую два числа (позиционные переменные и выполняющую их деление.\n'
+      'Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.')
+print('_' * 150)
+
+
+def func():
+    try:
+        a = float(input('Введите первое число: '))
+        b = float(input('Введите первое число: '))
+        c = round((a / b), 2)
+    except ZeroDivisionError:
+        c = "На ноль делить нельзя"
+    except ValueError:
+        c = "Вводить нужно цифры"
+    return c
+
+print(f"Деление двух этих чисел даст результат: {func()}")
