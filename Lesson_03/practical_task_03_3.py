a8f8ed826cec621fc332a037b88c88a6ffd1e941
Bessonov Alexandr <sabe84@mail.ru>
2020-01-22 17:02:19 +0300

    Третье практическое задание к третьему уроку выполнено

--- Lesson_03/practical_task_03_3.py
+++ Lesson_03/practical_task_03_3.py
@@ -1,4 +1,20 @@
 print("This is the solution to task 3 to lesson 3")
 print('_' * 150)
-print('Реализовать функцию my_func(), которая принимает три позиционных аргумента, и возвращает сумму наибольших двух аргументов.')
+print('Реализовать функцию my_func(), которая принимает три позиционных аргумента, '
+      'и возвращает сумму наибольших двух аргументов.')
 print('_' * 150)
+
+
+def func(var_1, var_2, var_3):
+    i = [var_1, var_2, var_3]
+    el_min = min(i)
+    i.remove(el_min)
+    return sum(i)
+
+
+num = []
+num.append(int(input("Введите первое число: ")))
+num.append(int(input("Введите второе число: ")))
+num.append(int(input("Введите третье число: ")))
+summa = func(num[0], num[1], num[2])
+print(f'Сумма двух наибольших числа равна: {summa}')
