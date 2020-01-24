0a41396424c7bbfaeb61ae5d3fc7e346fcdf2f54
Bessonov Alexandr <sabe84@mail.ru>
2020-01-24 08:38:13 +0300

    Практическое задание №6 к третьему уроку выполнено

--- Lesson_03/practical_task_06_3.py
+++ Lesson_03/practical_task_06_3.py
@@ -1,3 +1,11 @@
 print("This is the solution to task 6 to lesson 3")
 print('_' * 150)
 
+
+def int_func(a):
+    return a.title()
+
+
+print(int_func('text'))
+
+print(int_func(input('Введите строку из сдов разделенных пробелом: ')))
