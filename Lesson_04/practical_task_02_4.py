7b41d9dff793e8efa9b1e85cdbfc69aa92c29b65
Bessonov Alexandr <sabe84@mail.ru>
2020-01-26 17:21:40 +0300

    Пока не получается выполнить практического задания №2 к уроку №4 (потерял много времени, перехожу дальше)

--- Lesson_04/practical_task_02_4.py
+++ Lesson_04/practical_task_02_4.py
@@ -1,4 +1,10 @@
 print("This is the solution to task 2 to lesson 4")
-#print('_' * 150)
-#print('Реализовать')
-#print('_' * 150)
+# print('_' * 150)
+# print('')
+# print('_' * 150)
+
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
+new_list = [int(i) for i in my_list if i != 0 and my_list[i] > my_list[i-1]]
+
+print(new_list)
