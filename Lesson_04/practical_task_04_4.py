beb4482c4a119b13f425aee147922d4d7bc464c7
Bessonov Alexandr <sabe84@mail.ru>
2020-01-26 17:53:06 +0300

    Практическое задание №4 к уроку №4 выполнено

--- Lesson_04/practical_task_04_4.py
+++ Lesson_04/practical_task_04_4.py
@@ -1,2 +1,15 @@
 print("This is the solution to task 4 to lesson 4")
 print('_' * 150)
+from collections import Counter
+
+source = [300, 2, 12, 44, 1, 2, 1, 4, 10, 7, 1, 78, 2, 123, 55]
+print(source)
+my_list = []
+new_gen = [i for i in source if i not in my_list and (my_list.append(i) or True)]
+
+counter = Counter(source)
+
+new_gen = list(counter)
+
+single = [x for x, n in counter.items() if n == 1]
+print(single)
