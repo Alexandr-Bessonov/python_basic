4927b338e324a8e4d354d357a9d0e877481d43e6
Bessonov Alexandr <sabe84@mail.ru>
2020-01-27 13:49:31 +0300

    Практическое задание №2 к уроку №4 выполнено

--- Lesson_04/practical_task_02_4.py
+++ Lesson_04/practical_task_02_4.py
@@ -1,25 +1,23 @@
 print("This is the solution to task 2 to lesson 4")
-# print('_' * 150)
-# print('')
-# print('_' * 150)
+print('_' * 150)
 
 my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+print(my_list)
+new_list = [num for ind, num in enumerate(my_list) if ind != 0 and my_list[ind] > my_list[ind - 1]]
+
+print(new_list)
+
+print('_' * 150)
+print("Второй вариант решения")
 res_list = my_list[1:]
-new_list = []
+new_2_list = []
 
 k = int(0)
 while k < (len(my_list) - 1):
     if res_list[k] > my_list[k]:
-        new_list.append(res_list[k])
+        new_2_list.append(res_list[k])
         k += 1
     else:
         k += 1
 
-
-# new_list = [r for r in res_list for m in my_list if r > m]
-# ______________________________________
-# k = 1
-#new1_list = [el for el in my_list if my_list[el] > res_list[el-1]]
-
-print(new_list)
-#print(new1_list)
+print(new_2_list)
