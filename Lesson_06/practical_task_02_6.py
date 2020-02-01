ae67bb289290cf42cd40c05f27a5bcce44f2b01d
Bessonov Alexandr <sabe84@mail.ru>
2020-02-01 21:09:14 +0300

    В перерыве практическое задание №2 к уроку 6 выполнено

--- Lesson_06/practical_task_02_6.py
+++ Lesson_06/practical_task_02_6.py
@@ -1,7 +1,14 @@
 class Road:
+    __length = 0
+    __width = 0
+
     def __init__(self, length, width):
         self.length = length
         self.width = width
+    def mas(self):
+        massa = (int(self.length) * int(self.width) * 25 * 5) / 1000
+        return massa
 
 
 road = Road(input('Введите длину (в метрах): '), input('Введите ширину (в метрах0): '))
+print(f'Масса требуемого асфальтобетона: {road.mas()} тонн')
