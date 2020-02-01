2d4835b03759ded8c486982c6b222a53895ff491
Bessonov Alexandr <sabe84@mail.ru>
2020-02-01 19:55:16 +0300

    Второе опка не закончил, начинается вебинар по Linux, позже продолжу

---
+++ Lesson_06/practical_task_02_6.py
@@ -0,0 +1,7 @@
+class Road:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+
+road = Road(input('Введите длину (в метрах): '), input('Введите ширину (в метрах0): '))
