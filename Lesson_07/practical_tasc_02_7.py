a0f14ca6edf114b4179783a2937f6ec9c06db942
Bessonov Alexandr <sabe84@mail.ru>
2020-02-06 18:19:50 +0300

    Практическое задание №2 к уроку №7 выполнено

--- Lesson_07/practical_tasc_02_7.py
+++ Lesson_07/practical_tasc_02_7.py
@@ -5,28 +5,41 @@ class Clothes(ABC):
     def __init__(self, title="type of clothing"):
         self.title = title
 
-    @abstractmethod
-    def my_metod_1(self):
-        pass
-
-    @abstractmethod
-    def my_metod_2(self):
-        pass
+    # @abstractmethod
+    # def my_metod_1(self):
+    #     pass
+    #
+    # @abstractmethod
+    # def my_metod_2(self):
+    #     pass
+    # @property
+    # def sq_all(self):
+    #     return str(f'Общая площадь материала \n {self.sq_coat+ self.sq_costume}')
 
 
 class Coat(Clothes):
+    def __init__(self, title):
+        super().__init__(title)
+        self.sq_coat = round(self.title / 6.5 + 0.5)
 
-    def my_metod_1(self):
-        pass
-
-    def my_metod_2(self):
-        pass
+    def __str__(self):
+        return f'Площадь требуемого материала на пальто {self.sq_coat}'
 
 
 class Costume(Clothes):
 
-    def my_metod_1(self):
-        pass
+    def __init__(self, title):
+        super().__init__(title)
+        self.sq_costume = round(self.title * 2 + 0.3)
 
-    def my_metod_2(self):
-        pass
+    def __str__(self):
+        return f'Площадь требуемого материала на костюм {self.sq_costume}'
+
+coat = Coat(48)
+costume = Costume(1.72)
+print(coat)
+print(costume)
+print(coat.__str__())
+print(costume.__str__())
+
+# print(f'Общая площадь материала: {coat.__str__()+ costume.__str__()}')
